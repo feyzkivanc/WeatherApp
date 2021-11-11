09de451ba5becd5f865bf0d711195d76448a0a6d
feyzkivanc <feyzakivanc@gmail.com>
2021-11-11 16:55:50 +0300

    Weather

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    /*Koşullar :
+
+    Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+    Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+    Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+    Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.*/
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("lütfen bir sıcaklık değeri girin");
+        double heat= scan.nextDouble();
+        if(heat<5){
+            System.out.println("Kayak yapmaya gidebilirsiniz.");
+        }
+        if(heat>=5 && heat<=15 ){
+            System.out.println("Sinemaya gidebilirsiniz");
+        }
+        else if(heat>15 && heat<=25){
+            System.out.println("Pikniğe gidebilirsiniz");
+        }
+        else{
+            System.out.println("Yüzmeye gidebilirsiniz");
+        }
+
     }
 }
